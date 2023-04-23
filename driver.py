220bfd0c547dd5257305af261fdf946c4b9c1211
LucasHasting <110124874+LucasHasting@users.noreply.github.com>
2023-04-23 11:08:38 -0500

    Add files via upload

--- driver.py
+++ driver.py
@@ -1,22 +1,49 @@
 #https://www.geeksforgeeks.org/python-call-function-from-another-file/
 #https://www.geeksforgeeks.org/python-pil-image-open-method/s
 #https://stackoverflow.com/questions/41942109/plotting-the-digraph-with-graphviz-in-python-from-dot-file
-from PIL import Image
 from graphviz import Source
 from file_conversion import *
 
 def main():
-    #name input and output files
     input_file = "data.txt" 
     output_file = "data.dot"
-    show_file(input_file, output_file, True)
-    
+    Roads = FileConversion()
+    Roads.convert_files(input_file, output_file, False)
+
+    while True:
+            print("Options:")
+            print("1. Road with miles")
+            print("2. See road without miles")
+            print("3. Enter path")
+            print("4. Quit")
+            choice = input("Enter your choice (1-4): ")
+
+            print()
+
+            if choice == "1":
+                show_file(Roads, input_file, output_file, True)
+
+            elif choice == "2":
+                show_file(Roads, input_file, output_file, False)
+
+            elif choice == "3":
+                vertex_a, vertex_b = get_check_node(Roads)
+
+                if (vertex_a == 0):
+                    continue
+
+                Roads.split_paths(vertex_a, vertex_b)
+
+            elif choice == "4":
+                break
+
+            else:
+                print("Invalid choice. Please try again.")
 
     
-def show_file(inputf, outputf, weight=True):
+def show_file(Road, inputf, outputf, weight=True):
     #convert files for unweighted
-    Roads = FileConversion()
-    Roads.convert_files(inputf, outputf, weight)
+    Road.convert_files(inputf, outputf, weight)
 
     #set the path the dot file is in
     path = f"./{outputf}"
@@ -26,5 +53,18 @@ def show_file(inputf, outputf, weight=True):
 
     #view the dot file
     s.view()
+
+def get_check_node(Road):
+    vertex_a = input("What is the starting vertex: ")
+    if (not Road.check_nodes(vertex_a)):
+        print("invalid vertex")
+        return 0, 0
+
+    vertex_b = input("What is the ending vertex: ")
+    if (not Road.check_nodes(vertex_b)):
+        print("invalid vertex")
+        return 0, 0
+    
+    return vertex_a, vertex_b
     
 main()
--- file_conversion.py
+++ file_conversion.py
@@ -3,6 +3,11 @@
 #https://www.w3schools.com/python/python_file_write.asp
 #https://www.geeksforgeeks.org/python-remove-all-values-from-a-list-present-in-other-list/
 class FileConversion:
+    def __init__(self):
+        self.data_list = []
+        self.shortest_path = []
+        self.non_shortest_path = []
+    
     def convert_files(self, input_file, output_file, W=True):
         #open input file 
         input_file = open(f"./{input_file}", 'r')
@@ -12,47 +17,50 @@ class FileConversion:
         output_file = open(f"./{output_file}", 'w')
         output_file.write("digraph road{\n")
 
-        data_list = []
+        self.data_list = []
+        used_list = []
         
         #splits the input file into an array
         for i in input_file:
             i = i.split(",")
             i[2] = float(i[2])
-            data_list.append(i)
+            self.data_list.append(i)
 
-        #split the shortest path from other paths
-        self.split_paths(data_list)
+        
+        for i in self.data_list:
+            if ([i[1], i[0], i[2]] in used_list):
+                continue
 
-        #add the non shortest path edges
-        for i in self.non_shortest_path:
+            used_list.append(i)
+            
             if (W):
-                self.convert_to_dot_weight(i[0], i[1], i[2], output_file, "black")
+                if self.shortest_path in i:
+                    self.convert_to_dot_weight(i[0], i[1], i[2], output_file, "red")
+                    self.convert_to_dot(i[1], i[0], output_file, "black")
+                else:
+                    self.convert_to_dot_weight(i[0], i[1], i[2], output_file, "black")
+                    self.convert_to_dot(i[1], i[0], output_file, "black")
             else:
                 self.convert_to_dot(i[0], i[1], output_file, "black")
-            output_file.write("\n")
+                self.convert_to_dot(i[1], i[0], output_file, "black")
 
-        #add the shortest path edges
-        for i in self.shortest_path:
-            if (W):
-                self.convert_to_dot_weight(i[0], i[1], i[2], output_file, "red")
-            else:
-                self.convert_to_dot(i[0], i[1], output_file, "red")
             output_file.write("\n")
 
         #close the dot file
         output_file.write("}")
         output_file.close()
 
-    def split_paths(self, data):
+    def split_paths(self, A, B):
         #gets the shortest path
-        self.shortest_path = self.ShortestPath()
+        self.shortest_path = self.ShortestPath(A,B)
 
         #splits the shortest path from the main list
-        self.non_shortest_path = [i for i in data if i not in self.shortest_path]
+        self.non_shortest_path = [i for i in self.data_list if i not in self.shortest_path]
         
-    def ShortestPath(self):
-        #example path, leave weights
-        path = [["g","h", 10.0],["h","i", 5.0]] 
+    def ShortestPath(self, A, B):
+        #A and B are both variables that store a node
+        #example path
+        path = [["g","h"],["h","i"]] 
         return path
 
     def convert_to_dot_weight(self, A, B, weight, output, color):
@@ -61,7 +69,6 @@ class FileConversion:
          output.write(f"\"{B}\" [shape = circle]\n")
          output.write("\n")
          output.write(f"\"{A}\" -> \"{B}\" [label = {weight},color = \"{color}\"]\n")
-         output.write(f"\"{B}\" -> \"{A}\" [color = \"{color}\"]\n")
 
     def convert_to_dot(self, A, B, output, color):
          #write an edge (and node) to the dot file 
@@ -69,4 +76,10 @@ class FileConversion:
          output.write(f"\"{B}\" [shape = circle]\n")
          output.write("\n")
          output.write(f"\"{A}\" -> \"{B}\" [color = \"{color}\"]\n")
-         output.write(f"\"{B}\" -> \"{A}\" [color = \"{color}\"]\n")
+
+    def check_nodes(self, node):
+        for i in self.data_list:
+            if node in i:
+                return True
+        return False
+            
