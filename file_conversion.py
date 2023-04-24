73f74f504736a919a61741f4a8e6cf4c947625ea
ethannix <125933235+ethannix@users.noreply.github.com>
2023-04-24 17:04:40 -0500

    Update file_conversion.py

--- file_conversion.py
+++ file_conversion.py
@@ -68,12 +68,63 @@ class FileConversion:
         output_file.close()
         
     def ShortestPath(self, A, B):
+         # define a dictionary to store the graph
+    graph = {}
+    for i in self.data_list:
+        if i[0] not in graph:
+            graph[i[0]] = []
+        graph[i[0]].append((i[1], i[2]))
+        if i[1] not in graph:
+            graph[i[1]] = []
+        graph[i[1]].append((i[0], i[2]))
+        
+    # initialize variables
+    distances = {}
+    visited = set()
+    previous = {}
+    pq = []
+
+    # set start node distance to 0 and add to priority queue
+    distances[A] = 0
+    heapq.heappush(pq, (0, A))
+
+    # Dijkstra's Algorithm
+    while pq:
+        # get node with smallest distance from priority queue
+        current_distance, current_node = heapq.heappop(pq)
+
+        # if node has already been visited, continue to next node
+        if current_node in visited:
+            continue
+
+        # mark node as visited
+        visited.add(current_node)
+
+        # update distances to neighbors and add to priority queue
+        for neighbor, weight in graph[current_node]:
+            distance = current_distance + weight
+            if neighbor not in distances or distance < distances[neighbor]:
+                distances[neighbor] = distance
+                previous[neighbor] = current_node
+                heapq.heappush(pq, (distance, neighbor))
+
+    # construct shortest path from start to end
+    path = []
+    current_node = B
+    while current_node != A:
+        path.append(current_node)
+        current_node = previous[current_node]
+    path.append(A)
+    path.reverse()
+
+    self.shortest_path = path
+   
         #A and B are both variables that store a node
         #example path
-        A = [i for i in self.data_list if A in i]
-        print(A)
-        path = [["NWoodAvenue","NPineStreet"],["NPineStreet","starbucks"]] 
-        self.shortest_path = path
+        #A = [i for i in self.data_list if A in i]
+        #print(A)
+        #path = [["NWoodAvenue","NPineStreet"],["NPineStreet","starbucks"]] 
+        #self.shortest_path = path
 
     def convert_to_dot_weight(self, A, B, weight, output, color):
          #write an edge (and node) to the dot file 
